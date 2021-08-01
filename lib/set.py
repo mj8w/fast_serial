917b377103cfd54becc78c0e3f51a0189e4cb9c4
Micheal Wilson <mail4mikew@gmail.com>
2021-08-01 10:16:26 -0700

    save actions after up/down buttons

--- lib/set.py
+++ lib/set.py
@@ -14,7 +14,7 @@ settings = "\\".join([base_dir, settings_file])
 # get the default settings
 values = {
     'window_size':[500, 700],
-    'actions':{'help':'help<cr><lf>'},
+    'actions':[('help', 'help<cr><lf>')],
     'baud_rates':['1200', '2400', '4800', '9600', '19200', '38400', '57600', '115200'],
     'splitter_pos':[0, 258],
     'window_size':(167, 118, 1262, 367),
@@ -56,7 +56,7 @@ except:
     baud_rates = ['1200', '2400', '4800', '9600', '19200', '38400', '57600', '115200']
     splitter_pos = [0, 258]
     window_size = (167, 118, 1262, 367)
-    actions = {'help':'help<cr><lf>'}
+    actions = [('help', 'help<cr><lf>')]
     baud_rate = '115200'
     com_port = 'COM3'
     send_history = []
