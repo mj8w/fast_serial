151b6f8b5b718c283c84ce6632c2e78184a17303
Mike Wilson <mail4mikew@gmail.com>
2021-11-06 20:01:37 -0700

    update license info

--- lib/connect.py
+++ lib/connect.py
@@ -1,3 +1,22 @@
+"""
+Copyright 2021 Micheal Wilson
+Fast_serial project founded by Micheal Wilson
+
+    This file is part of Fast_Serial.
+
+    Fast_Serial is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 2 of the License, or
+    (at your option) any later version.
+
+    Fast_Serial is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Fast_Serial.  If not, see <https://www.gnu.org/licenses/>
+"""
 
 from PyQt5.QtCore import QThread
 from PyQt5.Qt import QTextCursor
